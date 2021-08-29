d298edb7fdd80b8e2a127d6ddefeba1dff8a4cda
Ryan Anderson <ryan@anderson.vc>
2021-08-29 17:22:48 -0400

    added optional annotation filtering for visdrone dataset

--- tools/train.py
+++ tools/train.py
@@ -4,8 +4,17 @@ import os
 from pathlib import Path
 import sys
 
-sys.path.append(str(Path('..').absolute().parent))
+from dotenv import load_dotenv
 
+load_dotenv()
+assert None not in {
+    os.getenv("BASE_DIR"),
+    os.getenv("MLFLOW_DATA_DIR"),
+    os.getenv("MLFLOW_MODEL_DIR"),
+}, "Not all environment variables set. Be sure to edit .env"
+sys.path.append(str(os.getenv("BASE_DIR")))
+
+import mlflow
 import mmcv
 from mmcv import Config
 from mmdet.datasets import build_dataset, build_dataloader
@@ -14,31 +23,38 @@ from mmdet.apis import train_detector
 
 from visdrone.datasets import VisDroneDataset
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
-parser = argparse.ArgumentParser(description='Train Custom Model')
-parser.add_argument('--config_file', help='path to mmdet training config file')
-parser.add_argument('--work_dir', help='file directory used to store model weights')
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.DEBUG
+)
+
+parser = argparse.ArgumentParser(description="Train Custom Model")
+parser.add_argument("--config_file", help="path to mmdet training config file")
+parser.add_argument("--experiment_name", help="assign experiment name")
+parser.add_argument(
+    "--work_dir",
+    help="parent directory to store experiment logs & weights",
+    default=os.environ.get("MFLOW_DATA_DIR"),
+)
 args = parser.parse_args()
 
-logging.info('loading config file')
+logging.info("loading config file")
 try:
     cfg = Config.fromfile(args.config_file)
 except Exception as e:
-    logging.error(' '.join(["failed to load config_file:",args.config_file]))
+    logging.error(" ".join(["failed to load config_file:", args.config_file]))
 cfg.work_dir = args.work_dir
 
 
-
-logging.info('loading datasets')
+logging.info("loading datasets")
 datasets = [build_dataset(cfg.data.train)]
 
-logging.info('generating model')
-model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'))
+logging.info("generating model")
+model = build_detector(cfg.model, train_cfg=cfg.get("train_cfg"))
 model.CLASSES = VisDroneDataset.CLASSES
+model.init_weights()
 
 # Create work_dir
 mmcv.mkdir_or_exist(os.path.abspath(cfg.work_dir))
-logging.info('starting model training loop')
+logging.info("starting model training loop")
 train_detector(model, datasets, cfg, distributed=False, validate=True)
-
--- visdrone/datasets.py
+++ visdrone/datasets.py
@@ -6,51 +6,92 @@ from mmdet.datasets import build_dataset
 from mmdet.datasets.builder import DATASETS
 from mmdet.datasets.custom import CustomDataset
 
-custom_dataset = 'VisDroneDataset'
+"""
+load_annotation tweaked to fix issue with custom dataset creation. 
+When resizing ground truth images, if the updated annotated bbox is too small, the localization loss will be NaN. 
+To fix this, we will ensure the bbox for the transformed image is >= 5x5 pixels.
+Additionally, if an image contains no instances, we will exclude it from the training / validation set.
+"""
+
+custom_dataset = "VisDroneDataset"
 if not DATASETS.get(custom_dataset):
+
     @DATASETS.register_module()
     class VisDroneDataset(CustomDataset):
-        
-        ALLOWED_TRUNCATION = 1 # 0=None, 1=1%-50%
-        ALLOWED_OCCLUSION = 1 # 0=None, 1=1%-50%, 2=50%-100%
-        CLASSES = ('Ignored','Pedestrian','People','Bicycle','Car','Van','Truck','Tricycle','Tricycle-awn','Bus','Motor','Others')
 
-        def __init__(self,**kwargs):
-            super().__init__(**kwargs,filter_empty_gt=False)
-        
-        def load_annotations(self,filepath):
+        ALLOWED_TRUNCATION = 1  # 0=None, 1=1%-50%
+        ALLOWED_OCCLUSION = 1  # 0=None, 1=1%-50%, 2=50%-100%
+        CLASSES = (
+            "Ignored",
+            "Pedestrian",
+            "People",
+            "Bicycle",
+            "Car",
+            "Van",
+            "Truck",
+            "Tricycle",
+            "Tricycle-awn",
+            "Bus",
+            "Motor",
+            "Others",
+        )
 
-            self.sample_filenames = [v.split('.')[0] for v in os.listdir(self.img_prefix)]
-            annotation_prefix = self.img_prefix.split('images')[0]+'/annotations'
+        def __init__(self, transform_dims=(1400, 788), **kwargs):
+            self.transform_dims = transform_dims
+            super().__init__(**kwargs, filter_empty_gt=True)
+
+        def load_annotations(self, filepath):
+
+            self.sample_filenames = [
+                v.split(".")[0] for v in os.listdir(self.img_prefix)
+            ][:100]
+            annotation_prefix = self.img_prefix.split("images")[0] + "/annotations"
             mmdet_annotations = []
-            for i,image_id in enumerate(self.sample_filenames):
-                if image_id=='':
+            for i, image_id in enumerate(self.sample_filenames):
+                if image_id == "":
                     continue
                 image_filename = f"{self.img_prefix}/{image_id}.jpg"
 
                 annotation_filename = f"{annotation_prefix}/{image_id}.txt"
                 img = Image.open(image_filename)
-                w,h = img.size
+                w, h = img.size
 
-                ann = pd.read_csv(annotation_filename,header=None)
+                ann = pd.read_csv(annotation_filename, header=None)
+
+                width_resize = self.transform_dims[0] / w
+                height_resize = self.transform_dims[1] / h
 
                 # Filter out occluded and truncated bounding boxes
-                ann = ann[(ann.iloc[:,6]<=self.ALLOWED_TRUNCATION) & (ann.iloc[:,7]<=self.ALLOWED_OCCLUSION)].reset_index(drop=True)
+                # Filter out reshaped images smaller than 5 pixels wide or 5 pixels high
+                ann = ann[
+                    (ann.iloc[:, 6] <= self.ALLOWED_TRUNCATION)
+                    & (ann.iloc[:, 7] <= self.ALLOWED_OCCLUSION)
+                    & (ann.iloc[:, 2] * width_resize >= 5)
+                    & (ann.iloc[:, 3] * height_resize >= 5)
+                ].reset_index(drop=True)
 
-                bboxes = np.array([ann.iloc[:,0],ann.iloc[:,1],ann.iloc[:,0]+ann.iloc[:,2],ann.iloc[:,1]+ann.iloc[:,3]],dtype=np.float32).T
-                labels = np.array(ann.iloc[:,5],dtype=int)
+                bboxes = np.array(
+                    [
+                        ann.iloc[:, 0],
+                        ann.iloc[:, 1],
+                        ann.iloc[:, 0] + ann.iloc[:, 2],
+                        ann.iloc[:, 1] + ann.iloc[:, 3],
+                    ],
+                    dtype=np.float32,
+                ).T
+                labels = np.array(ann.iloc[:, 5], dtype=int)
 
-                if labels.shape[0]==0 or labels.shape[0]>500:
+                if labels.shape[0] <= 1 or labels.shape[0] > 500:
                     continue
 
                 record = {
-                    'filename':image_filename.split('/')[-1],
-                    'width':w,
-                    'height':h,
-                    'ann': {
-                        'bboxes':bboxes,
-                        'labels':labels,
-                    }
+                    "filename": image_filename.split("/")[-1],
+                    "width": w,
+                    "height": h,
+                    "ann": {
+                        "bboxes": bboxes,
+                        "labels": labels,
+                    },
                 }
 
                 mmdet_annotations.append(record)
